164e29dbd1d6cecea9a1816f9019bfb100f4ba66
VICTOR <yo@casa.com>
2023-10-02 20:18:55 +0200

    04_pmm_kotlin_ejercicio_1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "04_PMM-Kotlin_Ejercicio_1"
+include(":app")
+ 
